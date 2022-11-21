db1ff50e39c43d6cd03cad0a7792bf14b4b1b834
Hillary <75864045+wein-hillary@users.noreply.github.com>
2022-11-21 14:56:47 -0600

    update header and className

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Whisk taker" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Recipes, fails, and exerpts from my kitchen.
         </p>
       </main>
 
