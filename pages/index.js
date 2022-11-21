080d4209fcb36191e93bf88b166e26ede59c4a05
Hillary <75864045+wein-hillary@users.noreply.github.com>
2022-11-21 16:13:11 -0600

    undo background image test

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,6 @@ export default function Home() {
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      body {
-      <background-image: url('https://lh3.googleusercontent.com/43RonhurXJTEEDInNW5gTMPNx08vG6VQyDLgTRYh20qBdngG1qRMV1swDjY43HYUt7QiP_Kqhxeme8b-YGSe6yJG2jw_GwJX1QRmIlsSLr_rgA-qpWiIc2cSgsWSat6edE1SFDQk=w2400')>
-      }
       <main>
         <Header title="Whisk taker" />
         <p className="description">
